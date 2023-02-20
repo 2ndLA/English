36b222b6ed2b62c0d6dbcf11ee5cd5e382a37236
eric.shady <darkercookies@gmail.com>
2023-02-20 21:44:31 +0800

    refactor(vitepress): refactor sidebar data using a recursive way

--- .vitepress/gitbooktoc.js
+++ .vitepress/gitbooktoc.js
@@ -5,9 +5,6 @@ class Node {
     this._text = title
     this._link = `/${path}`
     this._items = []
-    // custom fields
-    this._level = indent.length / 2
-    this._parent = null
   }
 
   get text() { return this._text }
@@ -22,39 +19,6 @@ class Node {
     }
   }
 
-  get level() { return this._level }
-
-  get parent() {
-    if (this.level === 0) {
-      return null
-    }
-    return this._parent
-  }
-
-  set parent(value) {
-    this._parent = value
-  }
-
-  get isRoot() { return this.level === 0 }
-
-  isAdjacentDeeper(node) { return this.level - node.level === 1 }
-
-  affiliate(node) {
-    node.parent = this
-    this._items.push(node)
-  }
-
-  findClosestParent(node) {
-    let temp = this
-    while (temp) {
-      if (temp.level === node.level) {
-        return temp.parent
-      } else {
-        temp = temp.parent
-      }
-    }
-  }
-
   toJSON() {
     return {
       text: this.text,
@@ -65,32 +29,34 @@ class Node {
   }
 }
 
+function constructNode(line, pattern, result) {
+  if (line.startsWith('*')) {
+    const match = line.match(pattern)
+    const { title, path } = match.groups
+    result.push(new Node(title, path))
+    return result
+  } else {
+    line = line.slice(2)
+    result[result.length - 1]._items = constructNode(line, pattern, result[result.length - 1]._items)
+    return result
+  }
+}
+
+function sidebar(lines, pattern, result = []) {
+  if (lines.length === 0) {
+    return result
+  } else {
+    return sidebar(lines.slice(1), pattern, constructNode(lines[0], pattern, result))
+  }
+}
+
 function readGitBookSidebar() {
   const gbSidebarContent = readFileSync('SUMMARY.md')
   const gbSidebarLines = gbSidebarContent.toString().split('\n')
   const pattern = /^(?<indent> *)\* \[(?<title>.*)\]\((?<path>.*)\.md\)$/
-  const sidebarData = []
-
-  let currentParent = null
-  for (const line of gbSidebarLines) {
-    const match = line.match(pattern)
-    if (!match || !match.groups) {
-      continue
-    }
-
-    const { title, path, indent } = match.groups
-    const current = new Node(title, path, indent)
-    if (current.isRoot) {
-      sidebarData.push(current)
-    } else {
-      if (!current.isAdjacentDeeper(currentParent)) {
-        currentParent = currentParent.findClosestParent(current)
-      }
-      currentParent.affiliate(current)
-    }
-    currentParent = current
-  }
-
+  const regex = /\*/
+  const filteredGbSidebarLines = gbSidebarLines.filter(line => regex.test(line))
+  const sidebarData = sidebar(filteredGbSidebarLines, pattern, [])
   return sidebarData
 }
 
